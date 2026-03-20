2ddd38f731163e7ae6d219d8a231096eaeda84bf
Nathan Juillerat <152260041+NathanPasTrop@users.noreply.github.com>
2026-03-20 13:21:36 +0100

    ajout classe principal avec saisi utilisateur

---
+++ src/main/java/ch/divtec/Calculator.java
@@ -0,0 +1,12 @@
+package ch.divtec;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Holla como estas ?");
+
+        Scanner lecteur = new Scanner(System.in);
+        System.out.println(lecteur.next());
+    }
+}
