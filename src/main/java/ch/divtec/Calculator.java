5d137f5ae7e5c0032c76349480d720448893b557
Nathan Juillerat <152260041+NathanPasTrop@users.noreply.github.com>
2026-03-20 14:17:53 +0100

    ajout fonction mult + div

--- src/main/java/ch/divtec/Calculator.java
+++ src/main/java/ch/divtec/Calculator.java
@@ -9,6 +9,12 @@ public class Calculator {
     public double sub(double a, double b) {
         return a - b;
     }
+    public double mult(double a, double b) {
+        return a * b;
+    }
+    public double div(double a, double b) {
+        return a / b;
+    }
 
     public static void main(String[] args) {
         System.out.println("Holla como estas ?");
