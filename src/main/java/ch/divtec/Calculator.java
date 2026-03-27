469bf413edc8671d39988ce6e51f01a804646e74
Nathan Juillerat <152260041+NathanPasTrop@users.noreply.github.com>
2026-03-27 11:47:29 +0100

    ajout factorielle + test pour cette fonction

--- src/main/java/ch/divtec/Calculator.java
+++ src/main/java/ch/divtec/Calculator.java
@@ -3,23 +3,78 @@ package ch.divtec;
 import java.util.Scanner;
 
 public class Calculator {
-    public double add(double a, double b) {
+
+    double add(double a, double b) {
         return a + b;
     }
-    public double subtract(double a, double b) {
+
+    double subtract(double a, double b) {
         return a - b;
     }
-    public double multiply(double a, double b) {
+
+    double multiply(double a, double b) {
         return a * b;
     }
-    public double divide(double a, double b) {
+
+    double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
         return a / b;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Holla como estas ?");
+    long factorial(double n) {
+        if (n != (int)n) {
+            throw new IllegalArgumentException("Number must be an integer");
+        }
+        if (n < 0) {
+            throw new IllegalArgumentException("Number must be positive");
+        }
 
-        Scanner lecteur = new Scanner(System.in);
-        System.out.println(lecteur.next());
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+
+        return result == 0 ? 1 : result;
+    }
+
+    public static void main(String[] args) {
+        double num1, num2 = 0.;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Allowed operators: + - * / !");
+        System.out.println("Enter the operation to compute :");
+
+        num1 = sc.nextDouble();
+        char operator = sc.next().charAt(0);
+        if (operator != '!') {
+            num2 = sc.nextDouble();
+        }
+
+        final Calculator calc = new Calculator();
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = calc.add(num1, num2);
+                break;
+            case '-':
+                result = calc.subtract(num1, num2);
+                break;
+            case '*':
+                result = calc.multiply(num1, num2);
+                break;
+            case '/':
+                result = calc.divide(num1, num2);
+                break;
+            case '!':
+                result = calc.factorial(num1);
+                break;
+            default:
+                System.out.println("Invalid operator.");
+                return;
+        }
+        System.out.println("The final result: " + num1 + " " + operator + " " + num2 + " = " + result);
     }
 }
--- src/test/java/ch/divtec/CalculatorTest.java
+++ src/test/java/ch/divtec/CalculatorTest.java
@@ -12,128 +12,182 @@ public class CalculatorTest {
     void addTwoPositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.add(1, 1), 2);
+        assertEquals(2, calculator.add(1, 1));
     }
 
     @Test
     void addTwoPositiveFloat() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.add(1.1, 1.1), 2.2);
+        assertEquals(2.2, calculator.add(1.1, 1.1));
     }
 
     @Test
     void addTwoNegativeIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.add(-1, -1), -2);
+        assertEquals(-2, calculator.add(-1, -1));
     }
 
     @Test
     void addTwoNegativeFloat() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.add(-1.1, -1.1), -2.2);
+        assertEquals(-2.2, calculator.add(-1.1, -1.1));
     }
 
     @Test
     void addOneNegativeOnePositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.add(1, -2), -1);
+        assertEquals(-1, calculator.add(1, -2));
     }
 
     @Test
     void addOneNegativeOnePositiveFloat() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.add(2.2, -1.1), 1.1);
+        assertEquals(1.1, calculator.add(2.2, -1.1));
     }
 
-    // test function sub
+    // test function subtract
     @Test
     void subtractTwoPositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.subtract(3, 1), 2);
+        assertEquals(2, calculator.subtract(3, 1));
     }
 
     @Test
     void subtractTwoPositiveFloat() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.subtract(3.2, 1.1), 2.1);
+        assertEquals(2.1, calculator.subtract(3.2, 1.1));
     }
 
     @Test
     void subtractTwoNegativeIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.subtract(-3, -2), -1);
+        assertEquals(-1, calculator.subtract(-3, -2));
     }
 
     @Test
     void subtractTwoNegativeFloat() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.subtract(-3.1, -1.2), -1.9);
+        assertEquals(-1.9, calculator.subtract(-3.1, -1.2), 0.000000000000001);
     }
 
     @Test
     void subtractOneNegativeOnePositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.subtract(1, -2), 3);
+        assertEquals(3, calculator.subtract(1, -2));
     }
 
     @Test
     void subtractOneNegativeOnePositiveFloat() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.subtract(2.1, -1.1), 3.2);
+        assertEquals(3.2, calculator.subtract(2.1, -1.1));
     }
 
-    // test
+    // test function multiply
     @Test
     void multiplyTwoPositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.multiply(3, 2), 6);
+        assertEquals(6, calculator.multiply(3, 2));
     }
 
     @Test
     void multiplyTwoNegativeIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.multiply(-3, -2), 6);
+        assertEquals(6, calculator.multiply(-3, -2));
     }
 
     @Test
     void multiplyNegativeOnePositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.multiply(2, -2), -4);
+        assertEquals(-4, calculator.multiply(2, -2));
     }
 
-    // test
+    @Test
+    void multiplyZeroPositiveIntegers() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(0, calculator.multiply(0, 2));
+    }
+
+    @Test
+    void multiplyZeroNegativeIntegers() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(0, (int)calculator.multiply(0, -2));
+        assertEquals(0,calculator.multiply(0, -2), 0.000000000000000000000000000000001);
+    }
+
+    // test function divide
     @Test
     void divideTwoPositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.divide(3, 2), 1.5);
+        assertEquals(1.5, calculator.divide(3, 2));
     }
 
     @Test
     void divideTwoNegativeIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.divide(-3, -2), 1.5);
+        assertEquals(1.5, calculator.divide(-3, -2));
     }
 
     @Test
     void divideNegativeOnePositiveIntegers() {
         Calculator calculator = new Calculator();
 
-        assertEquals(calculator.divide(2, -2), -1);
+        assertEquals(-1, calculator.divide(2, -2));
     }
+
+    // Test function factoriel
+    @Test
+    void factorialPositiveIntegers() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(6, calculator.factorial(3));
+    }
+
+    @Test
+    void factorialNegativeIntegers() {
+        Calculator calculator = new Calculator();
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.factorial(-3) ;
+        });
+
+        assertTrue(exception.getMessage().contains("Number must be positive"));
+    }
+
+    @Test
+    void factorialPositiveDouble() {
+        Calculator calculator = new Calculator();
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.factorial(3.5) ;
+        });
+
+        assertTrue(exception.getMessage().contains("Number must be an integer"));
+    }
+
+    @Test
+    void factorialZero() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(1, calculator.factorial(0));
+    }
+
+
 }
